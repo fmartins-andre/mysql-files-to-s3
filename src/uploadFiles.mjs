c85a52c2a4b20b4c8abd759fc9c1c95bf197bbcc
fmartins-andre <fmartins.andre@gmail.com>
2025-10-28 08:57:01 -0300

    inital

---
+++ src/uploadFiles.mjs
@@ -0,0 +1,46 @@
+import { promises as fs } from "fs"
+import encrypt from "./utils/encrypt"
+
+const uploadFiles = async (
+  localData,
+  localFolder,
+  firebaseData,
+  crypto_key
+) => {
+  const { ref, prefix, filesNames } = firebaseData
+
+  return new Promise(resolve => {
+    const uploadedFiles = []
+    let counter = 0
+    localData.forEach(async row => {
+      const fileName = `${row.id}.pdf`
+      const localFilePath = `${localFolder}/${fileName}`
+
+      try {
+        if (!filesNames.includes(fileName)) {
+          await fs.access(localFilePath)
+          const localFile = await fs.readFile(localFilePath)
+
+          const fileRef = ref.child(`${prefix}/${fileName}`)
+          await fileRef.put(Uint8Array.from(localFile))
+          const url = await fileRef.getDownloadURL()
+
+          uploadedFiles.push({
+            _id: row.id,
+            hash: `${encrypt("MD5", row.verification_code, crypto_key)}`,
+            encrypted_url: `${encrypt("AES", url, crypto_key)}`,
+          })
+        }
+      } catch (error) {
+        console.log(`::: Firebase : Error while uploading file: ${error}`)
+      }
+      counter++
+      if (counter === localData.length) {
+        console.log(`::: Firebase : All files were uploaded.`)
+        resolve(uploadedFiles)
+      }
+    })
+  })
+}
+
+export default uploadFiles
